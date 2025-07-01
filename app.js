be1868ef66a8bb2a03e3a564f1e0b104dfb4fbc7
Abhishek Gonnade <2022chb1037@iitrpr.ac.in>
2025-07-02 03:30:31 +0530

    design

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
 main()
 .then(()=>{
     console.log("connected to db");
@@ -20,6 +21,7 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("Hi , I am root");
